da934bacd969d2bf1f6f168980770fd0fc118b6e
Ashmhmd <ashfak25321@gmail.com>
2022-11-02 18:20:24 +0530

    adding login with jwt token

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { LoginService } from '../services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginData = {
+    username: '',
+    password: '',
+  };
+
+  constructor(private snack:MatSnackBar, private login:LoginService) { }
 
   ngOnInit(): void {
+  }
+
+  formSubmit() {
+    console.log("Login form Submitted");
+
+    if(this.loginData.username.trim()=='' || this.loginData.username == null) {
+      this.snack.open("Username is Required", 'Ok');
+      return;
+    }else if(this.loginData.password.trim()=='' || this.loginData.password==null) {
+      this.snack.open("Password is Required", 'Ok');
+      return;
+    }
+      //request to server to generate token
+      this.login.generateToken(this.loginData).subscribe(
+        (data:any)=>{
+          console.log('success');
+          console.log(data);
+
+          //Login...
+        },
+        (error)=>{
+          console.log("Error !");
+          console.log(error);
+        }
+      );
   }
 
 }
